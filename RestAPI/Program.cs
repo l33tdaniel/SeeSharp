72969efb2b5a816b5badb41d29acfd795333ff47
l33tDanelLocal <danielneugent@icloud.com>
2022-06-14 09:05:52 -0500

    first push for C#

---
+++ RestAPI/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace std
+{
+    class Program
+    {
+        HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.getToDoItems();
+
+        }
+        private async Task getToDoItems()
+        {
+            string response = await client.GetStringAsync("https://jsonplaceholder.typicode.com/todos");
+
+            Console.WriteLine(response);
+
+            List<Todo> todo = JsonConvert.DeserializeObject<List<Todo>>(response);
+            
+            foreach (var item in todo)
+            {
+                Console.WriteLine("Title " + item.title + " \n ");
+            }
+        }
+    }
+    
+    class Todo
+    {
+        public int userID { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+
+    }
+}
